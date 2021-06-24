733931f4b293741537ad04510bc6a79fb87a9dd2
Rafael Dantas <rafaeldantasfortes@gmail.com>
2021-06-23 21:27:29 -0300

    Revert "teste de email"
    This reverts commit 5133b27c9974b5bccb193d63e95582c4d2b3ca57.

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'roberto.ht2ml@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Siforme'),
+        'address' => env('MAIL_FROM_ADDRESS', 'site@agenciapni.com.br'),
+        'name' => env('MAIL_FROM_NAME', 'PNI Formaturas'),
     ],
 
     /*
